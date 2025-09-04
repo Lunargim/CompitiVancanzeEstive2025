11517f0e523aec95212bccd0e964c8cee01929af
Lunargim <loryluna2001@gmail.com>
2025-09-04 10:14:41 +0200

    last chores: fix some minor stuff

--- FightingGame/Assets/Scripts/Fight/FightingController.cs
+++ FightingGame/Assets/Scripts/Fight/FightingController.cs
@@ -1,82 +1,32 @@
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class FightingController : MonoBehaviour
 {
-    public Transform enemyPos;
-    private float _horizontalMove;
-    private float _verticalMove;
-    private Vector3 _playerPosition;
-    [SerializeField] private float _radius;
-    [SerializeField] private float _speed;
+    [SerializeField] private float _attackCoolDown = 0.5f;
+    private int _normalAttackDamage = 1;
+    private int _heavyAttackDamage = 2;
+    private string[] attackAnimations = { "Punch", "Charge Punch" };
+    private float _lastTimeAttack;
+
     private Animator _animator;
 
     public void Start()
     {
         _animator = GetComponent<Animator>();
     }
-
-    public void Update()
+    void PerfromAttack(int attackIndex)
     {
-        this.gameObject.transform.LookAt(enemyPos);
-
-        MoveVertical();
-        MoveHorizontal();
-    }
-    private void MoveVertical()
-    {
-        _playerPosition = _radius * Vector3.Normalize(this.transform.position - enemyPos.position) + enemyPos.position;
-        _verticalMove = Input.GetAxisRaw("Vertical");
-        if (Input.GetKey(KeyCode.S))
+        if (Time.deltaTime - _lastTimeAttack > _attackCoolDown)
         {
-            this.transform.position = _playerPosition;
-            transform.RotateAround(enemyPos.position, Vector3.up, _verticalMove);
-            _animator.SetBool("Walk Side", true);
+            _animator.Play(attackAnimations[attackIndex]);
+
+            int damage = _normalAttackDamage;
+
+            _lastTimeAttack = Time.deltaTime;
         }
-
-        if (Input.GetKey(KeyCode.W))
+        else
         {
-            this.transform.position = _playerPosition;
-            transform.RotateAround(enemyPos.position, Vector3.down, -_verticalMove);
-            _animator.SetBool("Walk Side", true);
-        }
 
-        if(!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
-        {
-            _animator.SetBool("Walk Side", false);
         }
     }
-
-    private void MoveHorizontal()
-    {
-        _horizontalMove = Input.GetAxisRaw("Horizontal");
-        if (Input.GetKey(KeyCode.D))
-        {
-            if (_radius > 1.1) 
-            {
-                _radius -= _speed;
-                transform.Translate(Vector3.forward * _speed * _horizontalMove);
-                _animator.SetBool("Walk Forward", true);
-            }
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            if(_radius < 8)
-            {
-                _radius += _speed;
-                transform.Translate(Vector3.back * _speed * -_horizontalMove);
-                _animator.SetBool("Walk Backward", true);
-            }
-        }
-
-        if(!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
-        {
-            _animator.SetBool("Walk Forward", false);
-            _animator.SetBool("Walk Backward", false);
-        }
-    }
-
-
 }
-
-
---
+++ FightingGame/Assets/Scripts/Fight/MovementController.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class MovementController : MonoBehaviour
+{
+    public Transform enemyPos;
+    private float _horizontalMove;
+    private float _verticalMove;
+    private Vector3 _playerPosition;
+    [SerializeField] private float _radius;
+    [SerializeField] private float _speed;
+    private Animator _animator;
+
+    public void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        this.gameObject.transform.LookAt(enemyPos);
+
+        MoveVertical();
+        MoveHorizontal();
+    }
+    private void MoveVertical()
+    {
+        _playerPosition = _radius * Vector3.Normalize(this.transform.position - enemyPos.position) + enemyPos.position;
+        _verticalMove = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.position = _playerPosition;
+            transform.RotateAround(enemyPos.position, Vector3.up, _verticalMove);
+            _animator.SetBool("Walk Side", true);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.position = _playerPosition;
+            transform.RotateAround(enemyPos.position, Vector3.down, -_verticalMove);
+            _animator.SetBool("Walk Side", true);
+        }
+
+        if(!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            _animator.SetBool("Walk Side", false);
+        }
+    }
+
+    private void MoveHorizontal()
+    {
+        _horizontalMove = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (_radius > 1.1) 
+            {
+                _radius -= _speed;
+                transform.Translate(Vector3.forward * _speed * _horizontalMove);
+                _animator.SetBool("Walk Forward", true);
+            }
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            if(_radius < 8)
+            {
+                _radius += _speed;
+                transform.Translate(Vector3.back * _speed * -_horizontalMove);
+                _animator.SetBool("Walk Backward", true);
+            }
+        }
+
+        if(!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            _animator.SetBool("Walk Forward", false);
+            _animator.SetBool("Walk Backward", false);
+        }
+    }
+
+
+}
+
+
